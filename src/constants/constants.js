a0d7d4a6ea6eba6c270b5957b4dd77aa30d6bbfa
Mehrdad Ghannad <mehrdad37ghannad@gmail.com>
2022-05-10 19:53:38 -0400

    update projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,12 +1,12 @@
 export const projects = [
   {
-    title: "E-Commerce Shop",
+    title: "OpenAI Chatbot",
     description:
-      "Fully responsive and functional e-commerce website made using React.js, Commerce.js, and Material UI. All items shown for sale are for website demo only.",
-    image: "/images/shop.jpg",
-    tags: ["React.js, ", "Commerce.js,", "Material UI"],
-    source: "https://github.com/MehrdadQ/e-commerce",
-    visit: "https://mehrdadq.github.io/e-commerce/",
+      "Chatbot made with OpenAI's API to send user prompts to the GPT-3 language model and display the results. Users can select the engine used to generate the responses.",
+    image: "/images/openai.png",
+    tags: ["React, ", "CSS, ", "Bootstrap"],
+    source: "https://github.com/MehrdadQ/shopify_frontend_challenge_deployment",
+    visit: "https://mehrdadq.github.io/shopify_frontend_challenge_deployment",
     id: 3,
   },
   {
@@ -14,7 +14,7 @@ export const projects = [
     description:
       "As someone who always has some background music while doing work, I made this music player containing my favorite (copyright free) lofi songs with React.js and SCSS.",
     image: "/images/music.png",
-    tags: ["React.js, ", "SCSS"],
+    tags: ["React, ", "SCSS"],
     source: "https://github.com/MehrdadQ/Music-Player",
     visit: "https://mehrdadq.github.io/Music-Player/",
     id: 2,
@@ -22,7 +22,7 @@ export const projects = [
   {
     title: "Dream Journal",
     description:
-      "Research has proven that recording your dreams will improve memory, reduce stress, and overall add to your quality of sleep. This Dream Journal allows users to record their dreams by saving the entries in the user's Local Storage. Users can also assign a date to each dream entry.",
+      "This Dream Journal allows users to record their dreams by saving the entries in the user's Local Storage. Users can also assign a date to each dream entry.",
     image: "/images/dream.png",
     tags: ["HTML, ", "CSS, ", "JavaScript"],
     source: "https://github.com/MehrdadQ/Dream-Journal",
@@ -32,7 +32,7 @@ export const projects = [
   {
     title: "Color Guesser",
     description:
-      "It usually takes some practice for new front-end web developers to get themselves comfortable with rgb values. This tool can be used to practice dealing with rgb values by guessing the rgb values of the background color, and it can also be fun to mess around with.",
+      "It usually takes some practice for new front-end web developers to get comfortable with rgb values. This tool can be used to practice dealing with rgb values by guessing the rgb values of the background color.",
     image: "/images/color.png",
     tags: ["HTML, ", "CSS, ", "JavaScript"],
     source: "https://github.com/MehrdadQ/Color-Guesser",
